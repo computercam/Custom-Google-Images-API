ecb2a05bc2632704ed081e2c30ad6de65eabb051
Cam Sanders <csanders@protonmail.com>
2018-05-08 15:16:16 -0500

    various fixes

--- index.js
+++ index.js
@@ -5,114 +5,111 @@ const puppeteer = require('puppeteer');
 
 http.createServer(function(req, res) {
 
-    res.setHeader('Content-Type', 'text/arr' );
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET');
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,text/arr');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+	res.setHeader('Content-Type', 'text/arr');
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET');
+	res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,text/arr');
+	res.setHeader('Access-Control-Allow-Credentials', true);
 
-    let responseData = null;
-    const q = url.parse(req.url, true).query;
+	let responseData = null;
+	const q = url.parse(req.url, true).query;
 
-    let gimgSearch = 'https://google.com/search?q=' + q.keywords + '&tbm=isch';
-    
-    // TODO: Ability to get the second or following page of results. Right now only allows for first 100
-    switch(q) {
-        case q.rimg:
-            gimgSearch += '&tbs=rimg:' + q.rimg;
-        case q.safe:
-            gimgSearch += '&safe=active';      
-    }
+	let gimgSearch = 'https://google.com/search?q=' + q.keywords + '&tbm=isch';
 
-    function cleanMetadata(arr) {
-	    const unnecessary = [
-		    'cb',
-		    'cl',
-		    'cr',
-		    'id',
-		    'isu',
-		    'itg',
-		    'ity',
-		    'oh',
-		    'ow',
-		    'pt',
-		    'rh',
-		    'rid',
-		    'rt',
-		    'ru',
-		    's',
-		    'sc',
-		    'st',
-		    'th',
-		    'tw',
-		    'selector',
-		    'ved'
-	    ];
-	    for ( let key of arr ) {
-		    for ( let property of unnecessary ) {
-			    delete key[property];
-		    }
-	    }
-	    return arr;
-    }
-    
-    puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] }).then(async browser => {
-            const page = await browser.newPage();
-            await page.goto(gimgSearch);
-            await page.waitForSelector('[data-ri="99"');
+	// TODO: Ability to get the second or following page of results. Right now only allows for first 100
+	switch ( q ) {
+		case q.rimg:
+			gimgSearch += '&tbs=rimg:' + q.rimg;
+		case q.safe:
+			gimgSearch += '&safe=active';
+	}
 
-            responseData = await page.evaluate(() => {
-                let nodeList = Array.from(document.querySelectorAll('#search [data-ri]'));
-                let data = [];
-                nodeList.forEach((node, index) => {
-                    data[index] = JSON.parse(node.lastElementChild.textContent);
-                    data[index].selector = '[data-ri="' + index + '"]';
-                    data[index].ved = node.dataset.ved;
-                });
-                return data;
-            });
-            await browser.close();
-        })
-        .then(() => {
-            // Getting related image links.
-            let requests = [];
-            responseData.forEach((item, index) => {
-                requests[index] = axios({
-                    timeout: 10000,
-                    baseURL: 'https://www.google.com/async/',
-                    url: '/imgrc',
-                    method: 'get',
-                    responseType: 'text',
-                    params: {
-                        q: q.keywords,
-                        ved: item.ved,
-                        vet: '1' + item + '..i',
-                        imgurl: item.ou,
-                        imgrefurl: item.ru,
-                        tbnid: item.id,
-                        docid: item.rid,
-                        imgdii: item.id,
-                        async: 'cidx:1,saved:0,lp:0,_id:irc_imgrc1,_pms:s,_fmt:pc'
-                    },
-                    headers: {
-                        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36'
-                    }
-                });
-            });
-            axios.all(requests).then(function(results) {
-                results.forEach((result, index) => {
-                  let rimg = result.data.match(/tbs=rimg:([A-Za-z0-9_-])+/gm);
-                  if (rimg === null) {
-                    responseData[index].rimg = false;
-                  } else {
-                    responseData[index].rimg = rimg[0].replace(/tbs=rimg:/gm, '');
-                  }
-                });
-            }).then(() => {
-	            responseData = cleanMetadata(responseData);
-	            res.write(JSON.stringify(responseData));
-                res.end();
-            });
-        });
+	function cleanMetadata(arr) {
+		const unnecessary = [
+			'cb',
+			'cl',
+			'cr',
+			'id',
+			'isu',
+			'itg',
+			'ity',
+			'oh',
+			'ow',
+			'pt',
+			'rh',
+			'rid',
+			'rt',
+			'ru',
+			's',
+			'sc',
+			'st',
+			'th',
+			'tw',
+			'ved'
+		];
+		for ( let key of arr ) {
+			for ( let property of unnecessary ) {
+				delete key[ property ];
+			}
+		}
+		return arr;
+	}
+
+	puppeteer.launch({ args: [ '--no-sandbox', '--disable-setuid-sandbox' ] }).then(async browser => {
+		const page = await browser.newPage();
+		await page.goto(gimgSearch);
+		await page.waitForSelector('[data-ri="99"]');
+		responseData = await page.evaluate(() => {
+			let nodeList = Array.from(document.querySelectorAll('#search [data-ri]'));
+			let data = [];
+			nodeList.forEach((node, index) => {
+				data[ index ] = JSON.parse(node.lastElementChild.textContent);
+				data[ index ].ved = node.dataset.ved;
+			});
+			return data;
+		});
+		await browser.close();
+	})
+	.then(() => {
+		// Getting related image links.
+		let requests = [];
+		responseData.forEach((item, index) => {
+			requests[ index ] = axios({
+				timeout: 10000,
+				baseURL: 'https://www.google.com/async/',
+				url: '/imgrc',
+				method: 'get',
+				responseType: 'text',
+				params: {
+					q: q.keywords,
+					ved: item.ved,
+					vet: '1' + item + '..i',
+					imgurl: item.ou,
+					imgrefurl: item.ru,
+					tbnid: item.id,
+					docid: item.rid,
+					imgdii: item.id,
+					async: 'cidx:1,saved:0,lp:0,_id:irc_imgrc1,_pms:s,_fmt:pc'
+				},
+				headers: {
+					'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36'
+				}
+			});
+		});
+		axios.all(requests).then(function(results) {
+			results.forEach((result, index) => {
+				let rimg = result.data.match(/tbs=rimg:([A-Za-z0-9_-])+/gm);
+				if( rimg === null ) {
+					responseData[ index ].rimg = false;
+				} else {
+					responseData[ index ].rimg = rimg[ 0 ].replace(/tbs=rimg:/gm, '');
+				}
+			});
+		}).then(() => {
+			responseData = cleanMetadata(responseData);
+			res.write(JSON.stringify(responseData));
+			res.end();
+		});
+	});
 
 }).listen(8080);
