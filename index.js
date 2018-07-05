fb53be8ea1be903f4ef484c8856ab4aabd5bbfd0
Cameron Sanders <csanders@protonmail.com>
2018-07-05 14:04:32 -0500

    add greenlock express

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const http = require('http')
+const https = require('https')
+const redirectHttps = require('redirect-https')
 const url = require('url')
 const axios = require('axios')
 const puppeteer = require('puppeteer')
+
+let responseData = ''
 
 function cleanMetadata (data, query) {
   let newData = []
@@ -52,170 +56,179 @@ function parseQuery (q) {
   return str
 }
 
-let responseData = ''
+function handler (req, res) {
+  res.setHeader('Content-Type', 'text/json')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET')
 
-http
-  .createServer(function (req, res) {
-    res.setHeader('Content-Type', 'text/json')
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.setHeader('Access-Control-Allow-Methods', 'GET')
+  if (req.url.match(/keywords|sbi/gm)) {
+    const q = url.parse(req.url, true).query
+    let gimgSearch = parseQuery(q)
 
-    if (req.url.match(/keywords|sbi/gm)) {
-      const q = url.parse(req.url, true).query
-      let gimgSearch = parseQuery(q)
+    puppeteer
+      .launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+      .then(async browser => {
+        const page = await browser.newPage()
 
-      puppeteer
-        .launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
-        .then(async browser => {
-          const page = await browser.newPage()
+        // the initial search
+        await page.goto(gimgSearch, { waitUntil: 'load' })
 
-          // the initial search
-          await page.goto(gimgSearch, { waitUntil: 'load' })
+        // Grab the images on the page
+        responseData = await page.evaluate(() => {
+          let nodeList = Array.from(
+            document.querySelectorAll('#search [data-ri]')
+          )
+          let data = []
+          nodeList.forEach((node, index) => {
+            data[index] = JSON.parse(node.lastElementChild.textContent)
+            data[index].ved = node.dataset.ved
+          })
+          return data
+        })
 
-          // Grab the images on the page
-          responseData = await page.evaluate(() => {
-            let nodeList = Array.from(
-              document.querySelectorAll('#search [data-ri]')
-            )
-            let data = []
-            nodeList.forEach((node, index) => {
-              data[index] = JSON.parse(node.lastElementChild.textContent)
-              data[index].ved = node.dataset.ved
+        let errors = await page.evaluate(() => {
+        // check to make sure there wasn't an error
+          let err = document.body.innerText
+          err = err.match(/not\ match|doesn\'t\ refer|image\ is\ not\ publicly/gm)
+          if (err !== null) {
+            return true
+          }
+        })
+
+        // If there were no errors, continue
+        if (errors !== true) {
+        // Search by image was specified
+          if (isNotEmpty(q.sbi)) {
+          // If sbi, search the page for a link regarding search by image images
+            gimgSearch = await page.evaluate(() => {
+              let err = document.body.innerText
+              // if it doesn't exist, throw an error
+              err = err.match(/not\ match|doesn\'t\ refer|image\ is\ not\ publicly/gm)
+              if (err === null) {
+                let href = document.querySelector('#topstuff > .card-section a').getAttribute('href')
+                return 'https://www.google.com' + href
+              } else {
+                return false
+              }
             })
-            return data
-          })
-
-          let errors = await page.evaluate(() => {
-            // check to make sure there wasn't an error
-            let err = document.body.innerText
-            err = err.match(/not\ match|doesn\'t\ refer|image\ is\ not\ publicly/gm)
-            if (err !== null) {
-              return true
-            }
-          })
-
-          // If there were no errors, continue
-          if (errors !== true) {
-            // Search by image was specified
-            if (isNotEmpty(q.sbi)) {
-              // If sbi, search the page for a link regarding search by image images
-              gimgSearch = await page.evaluate(() => {
+            // If we were able to find a link to search by image feature, continue
+            if (gimgSearch !== false) {
+              await page.goto(gimgSearch, { waitUntil: 'domcontentloaded' })
+              let findNext = await page.evaluate(() => {
+              // Evaluate the page for link regarding visually similar images
                 let err = document.body.innerText
-                // if it doesn't exist, throw an error
                 err = err.match(/not\ match|doesn\'t\ refer|image\ is\ not\ publicly/gm)
                 if (err === null) {
-                  let href = document.querySelector('#topstuff > .card-section a').getAttribute('href')
-                  return 'https://www.google.com' + href
+                  let next = { href: false, keywords: false }
+                  // Check if the visually similar link exists
+                  let el = document.querySelector('.iu-card-header')
+                  if (el !== null) {
+                    next.href = el.getAttribute('href')
+                  } else {
+                  // if it doesn't we can use the keywords instead
+                    let el = document.querySelector('#topstuff > .card-section')
+                    if (el !== null) {
+                      next.keywords = el.lastChild.querySelector('a').getAttribute('href')
+                      next.keywords = next.keywords.match(/q=([^&]+)/g)[0].replace(/q=/g, '')
+                    }
+                  }
+                  return next
                 } else {
                   return false
                 }
               })
-              // If we were able to find a link to search by image feature, continue
-              if (gimgSearch !== false) {
-                await page.goto(gimgSearch, { waitUntil: 'domcontentloaded' })
-                let findNext = await page.evaluate(() => {
-                  // Evaluate the page for link regarding visually similar images
-                  let err = document.body.innerText
-                  err = err.match(/not\ match|doesn\'t\ refer|image\ is\ not\ publicly/gm)
-                  if (err === null) {
-                    let next = { href: false, keywords: false }
-                    // Check if the visually similar link exists
-                    let el = document.querySelector('.iu-card-header')
-                    if (el !== null) {
-                      next.href = el.getAttribute('href')
-                    } else {
-                      // if it doesn't we can use the keywords instead
-                      let el = document.querySelector('#topstuff > .card-section')
-                      if (el !== null) {
-                        next.keywords = el.lastChild.querySelector('a').getAttribute('href')
-                        next.keywords = next.keywords.match(/q=([^&]+)/g)[0].replace(/q=/g, '')
-                      }
-                    }
-                    return next
-                  } else {
-                    return false
-                  }
-                })
-                // We have either keywords or a link to follow
-                if (findNext !== false && findNext.keywords !== false && findNext.href !== false) {
-                  if (findNext.keywords !== false) {
-                    gimgSearch = parseQuery({ keywords: findNext.keywords })
-                  } else {
-                    gimgSearch = 'https://www.google.com' + findNext.href
-                  }
-                  // go to the page
-                  await page.goto(gimgSearch, { waitUntil: 'load' })
-                  let responseDataSBI = await page.evaluate(() => {
-                    let nodeList = Array.from(
-                      document.querySelectorAll('#search [data-ri]')
-                    )
-                    let data = []
-                    // get the images
-                    nodeList.forEach((node, index) => {
-                      data[index] = JSON.parse(node.lastElementChild.textContent)
-                      data[index].ved = node.dataset.ved
-                    })
-                    return data
-                  })
-                  // add the SBI images to the original set
-                  responseData = responseData.concat(responseDataSBI)
+              // We have either keywords or a link to follow
+              if (findNext !== false && findNext.keywords !== false && findNext.href !== false) {
+                if (findNext.keywords !== false) {
+                  gimgSearch = parseQuery({ keywords: findNext.keywords })
+                } else {
+                  gimgSearch = 'https://www.google.com' + findNext.href
                 }
+                // go to the page
+                await page.goto(gimgSearch, { waitUntil: 'load' })
+                let responseDataSBI = await page.evaluate(() => {
+                  let nodeList = Array.from(
+                    document.querySelectorAll('#search [data-ri]')
+                  )
+                  let data = []
+                  // get the images
+                  nodeList.forEach((node, index) => {
+                    data[index] = JSON.parse(node.lastElementChild.textContent)
+                    data[index].ved = node.dataset.ved
+                  })
+                  return data
+                })
+                // add the SBI images to the original set
+                responseData = responseData.concat(responseDataSBI)
               }
             }
           }
-          await browser.close()
-        })
-        .then(() => {
-          // Getting related image links.
-          if (responseData.length > 0) {
-            let requests = []
-            responseData.forEach((item, index) => {
-              requests[index] = axios({
-                timeout: 10000,
-                baseURL: 'https://www.google.com/async/',
-                url: '/imgrc',
-                method: 'get',
-                responseType: 'text',
-                params: {
-                  q: q.keywords,
-                  ved: item.ved,
-                  vet: '1' + item + '..i',
-                  imgurl: item.ou,
-                  imgrefurl: item.ru,
-                  tbnid: item.id,
-                  docid: item.rid,
-                  imgdii: item.id,
-                  async: 'cidx:1,saved:0,lp:0,_id:irc_imgrc1,_pms:s,_fmt:pc'
-                },
-                headers: {
-                  'user-agent':
-                  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36'
+        }
+        await browser.close()
+      })
+      .then(() => {
+      // Getting related image links.
+        if (responseData.length > 0) {
+          let requests = []
+          responseData.forEach((item, index) => {
+            requests[index] = axios({
+              timeout: 10000,
+              baseURL: 'https://www.google.com/async/',
+              url: '/imgrc',
+              method: 'get',
+              responseType: 'text',
+              params: {
+                q: q.keywords,
+                ved: item.ved,
+                vet: '1' + item + '..i',
+                imgurl: item.ou,
+                imgrefurl: item.ru,
+                tbnid: item.id,
+                docid: item.rid,
+                imgdii: item.id,
+                async: 'cidx:1,saved:0,lp:0,_id:irc_imgrc1,_pms:s,_fmt:pc'
+              },
+              headers: {
+                'user-agent':
+              'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/65.0.3325.181 Chrome/65.0.3325.181 Safari/537.36'
+              }
+            })
+          })
+          axios
+            .all(requests)
+            .then(function (results) {
+              results.forEach((result, index) => {
+                let rimg = result.data.match(/tbs=rimg:([A-Za-z0-9_-])+/gm)
+                if (rimg === null) {
+                  responseData[index].rimg = false
+                } else {
+                  responseData[index].rimg = rimg[0].replace(/tbs=rimg:/gm, '')
                 }
               })
             })
-            axios
-              .all(requests)
-              .then(function (results) {
-                results.forEach((result, index) => {
-                  let rimg = result.data.match(/tbs=rimg:([A-Za-z0-9_-])+/gm)
-                  if (rimg === null) {
-                    responseData[index].rimg = false
-                  } else {
-                    responseData[index].rimg = rimg[0].replace(/tbs=rimg:/gm, '')
-                  }
-                })
-              })
-              .then(() => {
-                responseData = cleanMetadata(responseData, q)
-                res.write(JSON.stringify(responseData))
-                res.end()
-              })
-          } else {
-            res.end()
-          }
-        })
-    } else {
-      res.end()
-    }
-  })
-  .listen(8080)
+            .then(() => {
+              responseData = cleanMetadata(responseData, q)
+              res.write(JSON.stringify(responseData))
+              res.end()
+            })
+        } else {
+          res.end()
+        }
+      })
+  } else {
+    res.end()
+  }
+}
+
+require('greenlock-express').create({
+  version: 'draft-11',
+  server: 'https://acme-v02.api.letsencrypt.org/directory',
+  // Note: If at first you don't succeed, stop and switch to staging
+  // https://acme-staging-v02.api.letsencrypt.org/directory
+  email: 'csanders@protonmail.com',
+  agreeTos: true,
+  approveDomains: [ 'gimgmetadata.limitunknown.com', 'www.gimgmetadata.limitunknown.com' ],
+  configDir: '~/.config/acme/',
+  app: require('express')().use('/', handler(req, res),
+  communityMember: true
+}).listen(80, 443)
