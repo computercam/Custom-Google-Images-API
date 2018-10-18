47fb95dca6b665fdee2e602e95d51a9076d7b1b6
Cameron Sanders <csanders@protonmail.com>
2018-10-18 11:18:16 -0500

    Added support for config.js to setup script

--- setup.sh
+++ setup.sh
@@ -5,7 +5,105 @@ if [[ $EUID -ne 0 ]]; then
     exit 1
 fi
 
+boolPrompt() {
+    # $1 = Var Name
+    # $2 = Var Prompt
+    # Usage = boolPrompt VAR_NAME PROMPT
+
+    local PROMPT_ANSWER=""
+    local PROMPT_VALIDATE="n"
+    
+    while [ "${PROMPT_VALIDATE}" == "n" ]
+    do
+        local BOOL="false"
+
+        echo -e "\n"    
+        echo -e "${2}"
+        echo -e "Enter (y)es or (n)o\n"
+        
+        read PROMPT_ANSWER 
+    
+        if [[ "${PROMPT_ANSWER}" == "y" ]]; then
+            local BOOL="true"
+        fi
+
+        clear 
+        echo -e "\n"  
+        echo -e "Are you sure you meant to enter ${PROMPT_ANSWER} for ${1}"
+        echo -e "Enter (y)es or (n)o\n"
+        
+        read PROMPT_VALIDATE
+         
+        if [[ "${PROMPT_VALIDATE}" != "y" ]]; then  
+            local PROMPT_VALIDATE="n"
+        fi
+        clear
+    done   
+
+    local  __return_var=$1
+    local  return_val=${BOOL}
+    eval $__return_var="'$return_val'"
+}
+
+stringPrompt() {
+    # $1 = Var Name
+    # $2 = Var Prompt
+    # Usage = stringPrompt VAR_NAME PROMPT
+
+    local PROMPT_ANSWER=""
+    local PROMPT_VALIDATE="n"
+    
+    while [ "${PROMPT_VALIDATE}" == "n" ]
+    do
+
+        echo -e "\n"    
+        echo -e "${2}"
+      
+        read PROMPT_ANSWER 
+    
+        clear
+        echo -e "\n"  
+        echo -e "Are you sure you meant to enter ${PROMPT_ANSWER} for ${1}"
+        echo -e "Enter (y)es or (n)o\n"
+        
+        read PROMPT_VALIDATE
+         
+        if [[ "${PROMPT_VALIDATE}" != "y" ]]; then              
+            local PROMPT_VALIDATE="n"
+        fi
+        clear
+    done   
+
+    local  __return_var=$1
+    local  return_val=${PROMPT_ANSWER}
+    eval $__return_var="'$return_val'"
+}
+
+boolPrompt LOCAL "Are you testing locally or don't want / can't use HTTPS. (Not running from a FQDN)"
+
+if [${LOCAL}];
+    then
+    sed -i "s/\"local\": false/\"local\": true/g" config.json
+    else
+    stringPrompt ACME_EMAIL "Enter your email. (me@email.com)"
+    stringPrompt FQDN_DOMAIN "Please enter a fully qualified domain name.\nThe domain name should have valid DNS records for both the domain and the www. subdomain.\nFailure to validate DNS will cause this script to fail. (mywebsite.com)"
+    sed -i "s/__ACME_EMAIL__/${ACME_EMAIL}/g" config.json
+    sed -i "s/__FQDN_DOMAIN__/${FQDN_DOMAIN}/g" config.json
+fi
+
+boolPrompt RESTRICT "Do you want to restrict requests to this api to only a specific domain? (Only mycoolapp.com can call this api)"
+
+if [${RESTRICT}];
+    then
+    stringPrompt ALLOW_ORIGIN_DOMAIN "Which domain should be allowed to request from this api?"
+    else
+    ALLOW_ORIGIN_DOMAIN="*"
+fi
+
+sed -i "s/__ALLOW_ORIGIN_DOMAIN__/${ALLOW_ORIGIN_DOMAIN}/g" config.json
+
 # Packages needed for chromium
+clear
 echo -e "Installing Dependencies \n"
 
 sudo apt-get update 
@@ -16,16 +114,19 @@ libxcursor1 libxdamage1 libxext6 libxfixes3 libxi6 libxrandr2 libxrender1 libxss
 ca-certificates fonts-liberation libappindicator1 libnss3 lsb-release xdg-utils wget
 
 # Install modules
+clear
 npm install
 
 # Install Systemd Service
+clear
 echo -e "Installing Application \n"
 
-PUB_IP='$(curl http://ipecho.net/plain; echo)'
+PUB_IP=$(curl http://ipecho.net/plain; echo)
 PWD=$(pwd | sed 's/\//\\\//g')
 NODE=$(which node | sed 's/\//\\\//g')
 START_COMMAND="${NODE} ${PWD}\/index.js"
 
+# Systemd service
 sed -i "s/__PWD__/${PWD}/g" gimgmetadata.service
 sed -i "s/__START_COMMAND__/${START_COMMAND}/g" gimgmetadata.service
 
@@ -34,6 +135,7 @@ systemctl enable gimgmetadata.service
 systemctl start gimgmetadata.service
 
 # Information
+clear
 echo -e "Scraper Installed :)"
 echo -e "\nTest it online at:"
 echo -e "http://${PUB_IP}/?keywords=saturn+rain"
